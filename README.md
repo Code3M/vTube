4f3291469bdad458492b2e5b5e78f765cd52fd0e
Mahesh Das <maheshdasoffice9@gmail.com>
2024-01-09 23:46:25 +0530

    MongoDB_atlas setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vTube
+
+
+//mahesh@mongodb - maheshDB
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-var a = 55
 
-console.log(a);
+import 'dotenv/config'
+import connectDB from './db/index.js'
+/*
+//Approch 1 to connect with DB
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js"
+import express from "express"
+const app = express()
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.error("Error", error)
+            throw error
+        })
+    } catch (error) {
+        console.error("Error ", error)
+        throw error
+    }
+})()
+*/
+
+connectDB()
